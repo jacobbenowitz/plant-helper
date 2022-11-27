f95432f99001c7db3569fc708f011f1a5a6e0a9a
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-11-27 13:14:28 -0500

    setup api routes

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const picoMaker = require('./api/pico-maker');
+const pico2 = require('./api/pico-2');
+const bodyParser = require('body-parser');
 const app = express();
 const db = require('./config/keys').mongoURI;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -10,6 +16,8 @@ mongoose
 
 
 app.get('/', (req, res) => { res.send("Hello World!"); });
+app.use('/api/pico-maker', picoMaker);
+app.use('/api/pico-2', pico2);
 
 const port = process.env.PORT || 5001;
 
